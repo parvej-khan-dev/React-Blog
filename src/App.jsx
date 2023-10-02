72bfe2967f3264e5c719791d724dcf403e190428
Amrit <inquisitive.developer88@gmail.com>
2023-10-02 17:27:42 +0530

    redux setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Footer, Header } from "./components";
+import { Outlet } from "react-router-dom";
+import authService from "./appwrite/auth";
+import { useDispatch } from "react-redux";
+import {login, logout} from "./store/reducers/authSlice"
+ 
+ 
 function App() {
-  const [count, setCount] = useState(0)
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  const [isLoading, setLoading] = useState(true)
 
+    const dispatch = useDispatch()
+   
+  useEffect(()=> {
+      authService.getCurrentUser ()
+      .then((userData) => {
+        if(userData){
+          dispatch(login({userData}))
+          setLoading(false)
+        }else{
+          dispatch(logout())
+        }
+       
+      })
+      .finally(
+         setLoading(false)
+      )
+
+     
+    }
+  ,[])
+
+  if(!isLoading) return <div>Loading...</div>
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Header />
+        <Outlet />
+        <Footer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/appwrite/config.js
@@ -0,0 +1,131 @@
+import { Databases, Storage, ID, Query, Client } from "appwrite";
+import conf from "../conf/conf";
+
+export class Service {
+  Client = new Client();
+  databases;
+  bucket;
+  constructor() {
+    this.client
+      .setEndpoint(conf.appWriteUrl)
+      .setProject(conf.appWriteProjectId);
+
+    this.databases = new Databases(this.client);
+    this.bucket = new Storage(this.client);
+  }
+
+  async createPost({ title, slug, content, image, status, userId }) {
+    try {
+      return await this.databases.createDocument(
+        conf.appWriteDatabaseId,
+        conf.appWriteCollectionId,
+        slug,
+        {
+          title,
+          content,
+          image,
+          status,
+          userId,
+        }
+      );
+    } catch (error) {
+      console.log("Appwrite Error::create post", error);
+    }
+  }
+
+  // update document
+
+  async updatePost(slug, { title, content, image, status }) {
+    try {
+      return await this.databases.updateDocument(
+        conf.appWriteDatabaseId,
+        conf.appWriteCollectionId,
+        slug,
+        {
+          title,
+          content,
+          image,
+          status,
+        }
+      );
+    } catch (error) {
+      console.log("Appwrite Error::update post", error);
+    }
+  }
+
+  // delete document
+  async deletePost(slug) {
+    try {
+      await this.databases.deleteDocument(
+        conf.appWriteDatabaseId,
+        conf.appWriteCollectionId,
+        slug
+      );
+      return true;
+    } catch (error) {
+      console.log("Appwrite Error::delete post", error);
+    }
+    return false;
+  }
+
+  // get documents
+  async getPostById() {
+    try {
+      return await this.databases.getDocument(
+        conf.appWriteDatabaseId,
+        conf.appWriteCollectionId,
+        slug
+      );
+    } catch (error) {
+      console.log("Appwrite Error::get post", error);
+    }
+  }
+
+  // list documents that status is Active
+  async listPosts(queries = [Query.equal("status", "Active")]) {
+    try {
+      return await this.databases.listDocuments(
+        conf.appWriteDatabaseId,
+        conf.appWriteCollectionId,
+        queries
+      );
+    } catch (error) {
+      console.log("Appwrite Error::list post", error);
+      return false;
+    }
+  }
+
+  // file upload services
+  async uploadFile(file) {
+    try {
+      return await this.bucket.createFile(
+        conf.appWriteBucketId,
+        ID.unique(),
+        file
+      );
+    } catch (error) {
+      console.log("Appwrite Error::upload file", error);
+      return false;
+    }
+  }
+
+  // delete file
+
+  async deleteFile(fileId) {
+    try {
+      await this.bucket.deleteFile(conf.appWriteBucketId, fileId);
+      return true;
+    } catch (error) {
+      console.log("Appwrite Error::delete file", error);
+      return false;
+    }
+  }
+
+  // get file preview
+  getFilePreview(fileId) {
+    return this.bucket.getFilePreview(conf.appWriteBucketId, fileId);
+  }
+}
+
+const service = new Service();
+export default service;
